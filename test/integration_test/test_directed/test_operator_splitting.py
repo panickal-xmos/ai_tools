8f62262a30b41035f2b50f3b92b8eea2397dc26b
scott-xmos <scottroset@xmos.com>
2021-12-08 10:49:08 -0500

    op splitting with 3x3 filter, padding, striding

--- test/integration_test/test_directed/test_operator_splitting.py
+++ test/integration_test/test_directed/test_operator_splitting.py
@@ -27,22 +27,39 @@ from .. import IntegrationTestModelGenerator, OperatorSplittingTestRunner
 
 class OperatorSplittingModelGenerator(IntegrationTestModelGenerator):
 	def _build_core_model(self) -> tf.keras.Model:
-		# inputs = tf.keras.Input(shape=(160,160, 3),)
-		# x0 = tf.keras.layers.Conv2D(3,(1, 1), strides=(1,1),activation='relu',padding='valid')(inputs)
-		# x1 = tf.keras.layers.Conv2D(3,(1, 1), strides=(1,1),activation='relu',padding='valid')(x0)
-		# # x_skip = x0
-		# # x1 = tf.keras.layers.DepthwiseConv2D(3,padding='same')(x0)
-		# # x2 = tf.keras.layers.Conv2D(16,1)(x1)
-		# # x3 = tf.keras.layers.Add()([x2,x_skip])
-		# x4 = tf.keras.layers.Flatten()(x1)
-		# outputs = tf.keras.layers.Dense(10, activation='relu')(x4)
+		inputs = tf.keras.Input(shape=(160,160, 3),)
 
-		# model = tf.keras.Model(inputs=inputs,outputs=outputs)
-		model = tf.keras.Sequential()
-		model.add(tf.keras.Input(shape=(160,160, 3),))
-		model.add(tf.keras.layers.Conv2D(3,(3, 3), strides=(1,1),activation='relu',padding='same'))
-		model.add(tf.keras.layers.Flatten())
-		model.add(tf.keras.layers.Dense(10, activation='relu'))
+		x = tf.keras.layers.Conv2D(16,(3, 3), strides=(2,2),padding='same')(inputs)
+		x_skip = x
+
+		x = tf.keras.layers.DepthwiseConv2D(16,padding='same')(x)
+		x = tf.keras.layers.Conv2D(16,1)(x)
+		x = tf.keras.layers.Add()([x,x_skip])
+
+		x = tf.keras.layers.Conv2D(96,(1, 1), padding='same')(x)
+		x = tf.keras.layers.DepthwiseConv2D(3,strides=(2,2),padding='same')(x)
+		x = tf.keras.layers.Conv2D(16,1)(x)
+		x_skip = x
+		
+		x = tf.keras.layers.Conv2D(96,(1, 1), padding='same')(x)
+		x = tf.keras.layers.DepthwiseConv2D(3,padding='same')(x)
+		x = tf.keras.layers.Conv2D(16,1)(x)
+
+		x = tf.keras.layers.Add()([x,x_skip])
+
+		x = tf.keras.layers.Flatten()(x)
+		outputs = tf.keras.layers.Dense(10, activation='relu')(x)
+
+		model = tf.keras.Model(inputs=inputs,outputs=outputs)
+
+		# model = tf.keras.Sequential()
+		# model.add(tf.keras.Input(shape=(10,10, 16),))#80,40,20,10,5
+		# model.add(tf.keras.layers.Conv2D(96,(1, 1), strides=(1,1),activation='relu',padding='same'))
+		# # model.add(tf.keras.layers.DepthwiseConv2D((3, 3), strides=(2,2),activation='relu',padding='same'))
+		# model.add(tf.keras.layers.DepthwiseConv2D((3, 3), strides=(1,1),activation='relu',padding='same'))
+		# model.add(tf.keras.layers.Conv2D(16,(1, 1), strides=(1,1),activation='relu',padding='same'))
+		# model.add(tf.keras.layers.Flatten())
+		# model.add(tf.keras.layers.Dense(10, activation='relu'))
 
 		return model
 
@@ -62,7 +79,7 @@ RUNNER = OperatorSplittingTestRunner
 @pytest.mark.skip_on_device
 def test_converted_model(operator_splitting_model: XCOREModel, experimental_xformer2: bool) -> None:
 	opcode_cnt = operator_splitting_model.count_operator_codes()
-	assert opcode_cnt[OperatorCode(BuiltinOpCodes.CONCATENATION)] == 1
+	assert opcode_cnt[OperatorCode(BuiltinOpCodes.CONCATENATION)] == 2
     
 if __name__ == "__main__":
     pytest.main()
